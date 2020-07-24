262ed0ddd0cd022f5ab2c6987d9732a46d8073f1
Ilya Nurullin <nurullinin@mail.ru>
2020-07-24 20:27:53 +0500

    feat: add start point layout

--- src/router.js
+++ src/router.js
@@ -21,6 +21,24 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/catalog",
+      name: "catalog",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/Catalog.vue")
+    },
+    {
+      path: "/cart",
+      name: "cart",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/Cart.vue")
     }
   ]
 });
